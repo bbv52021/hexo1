48c1df3e0db0338234ad551fe8a67cbcdc94288a
bbv52021 <bbv52021@users.noreply.github.com>
2025-02-26 05:34:37 +0000

    deploy: 5478084eaf6a3dffe7001d1c29e69e8348d3acec

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1740546631525";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1740548075987";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
