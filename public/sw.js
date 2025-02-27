b3987c1d654156363979eabea27972309249495c
bbv52021 <371363606@qq.com>
2025-02-27 20:34:23 +0800

    测试自动推送

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1740659457055";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1740659641068";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
